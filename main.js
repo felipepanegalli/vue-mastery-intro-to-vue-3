2baa3232c91abf404c656446a5071cf1c72a6772
Felipe Panegalli <felipe.panegalli@gmail.com>
2020-10-31 19:00:10 -0300

    Class 04, 05 and 06 done

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const app = Vue.createApp({
       inventory: 30,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'blue' },
-        { id: 2235, color: 'green' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
       ],
       cart: 0,
     };
@@ -15,6 +15,9 @@ const app = Vue.createApp({
   methods: {
     addToCart() {
       this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
     },
   },
 });
