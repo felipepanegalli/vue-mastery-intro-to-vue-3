d791e55a2cc077e11dbed0c1d18f56cc4679344b
Felipe Panegalli <felipe.panegalli@gmail.com>
2020-10-31 19:09:20 -0300

    Class 07 done

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inventory: 30,
+      inStock: false,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
